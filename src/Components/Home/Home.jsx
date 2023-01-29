49c60395816b1ea2dc4870c50793a36d102f1184
david temitope <temi236topemicheal@gmail.com>
2023-01-29 18:29:42 +0100

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import Button from "../UI/Button/Button";
+import AuthContext from "../Store/auth-context.jsx";
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
+
+const Home = () => {
+  const context = useContext(AuthContext);
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome back!</h1>
+      <Button onClick={context.onLogout}> logout</Button>
+    </Card>
+  );
+};
+
+export default Home;
